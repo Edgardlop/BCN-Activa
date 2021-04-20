5234821a284678759fc113cef3e7b052a4a40ae2
Néstor Edgardo López Martinez <46787568+Edgardlop@users.noreply.github.com>
2021-04-19 23:12:25 -0300

    Tasca -04

---
+++ php/footer.php
@@ -0,0 +1,4 @@
+<?php
+echo "<center><p>Copyright &copy; " . date("Y") . "  IT Academy BCN</p>";
+//comentario de Prueba git add 
+?>
---
+++ php/header.php
@@ -0,0 +1,3 @@
+<?php
+echo "<center><h1>Hoy es ". date('l jS \of F Y h:i:s A'. "</h1>");
+?>
